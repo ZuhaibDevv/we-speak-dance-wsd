d17bdeaaddf4f2bbd6a7385d55a15d77e12bded7
abdulmoizjawed <amoizj16@gmail.com>
2023-03-26 19:42:54 +0500

    Initial Commit With Tailwind Installed

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView >
+      <Text className="h-screen bg-black text-white">Initial Commit</Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
