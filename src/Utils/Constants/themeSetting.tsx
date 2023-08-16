1c4ca74932f6ed4d78a0cc0cea54091a4860ae65
Zuhaib Ahmed <100796522+ZuhaibDevv@users.noreply.github.com>
2023-08-16 16:42:45 -0700

    updated stack

--- src/Utils/Constants/themeSetting.tsx
+++ src/Utils/Constants/themeSetting.tsx
@@ -1,21 +1,18 @@
 const COLORS = {
-    blue: "#18A0FB",
-    primary: "#18A0FB",
-    green: "#178D1D",
-    white: "#ffffff",
-    borderColor: '#E0E0E0',
-    grey: "grey",
-    inActiveGrey: "#636161",
-    black: '#000000',
-    homeGrey: '#455A64',
-    lightgrey:'#BBBBBB',
-    yellow: '#FFC107',
-    orange: '#CC4A17',
-    orangeDisabled: '#a4390f',
-    placeholderColor: '#666664'
-    
-}
+  blue: '#18A0FB',
+  primary: '#18A0FB',
+  green: '#178D1D',
+  white: '#ffffff',
+  borderColor: '#E0E0E0',
+  grey: 'grey',
+  inActiveGrey: '#636161',
+  black: '#000000',
+  homeGrey: '#455A64',
+  lightgrey: '#BBBBBB',
+  yellow: '#FFC107',
+  orange: '#CC4A17',
+  orangeDisabled: '#a4390f',
+  placeholderColor: '#666664',
+};
 
-
-
-export { COLORS }
+export {COLORS};
